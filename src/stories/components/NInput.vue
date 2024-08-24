0fff9772a6c42484a437ae9356f444c2bcfe2a0a
FelipeGomeees <felipegomes.dsn@gmail.com>
2024-08-24 19:28:04 -0300

    feat: NInput basic variation added simples single bottom border variation of the ninput

--- src/stories/components/NInput.vue
+++ src/stories/components/NInput.vue
@@ -4,62 +4,61 @@
         v-bind="$attrs"
         type="text" 
         placeholder=""
-        :class="classes"
+        :class="inputClasses"
         />
         <label class="n-input__label">{{ label }}</label>
         <small class="n-input__small">{{ small }}</small>
     </div>
 </template>
   
-  <script>
-  import './ninput.css';
-  import { reactive, computed } from 'vue';
+<script>
+import './ninput.css';
+import { reactive, computed } from 'vue';
 
-//   defineOptions({
-//   inheritAttrs: false
-// })
-  
-  export default {
-    name: 'NInput',
-  
-    props: {
-      label: {
-        type: String,
-      },
-      small: {
-        type: String,
-      },
-      primary: {
-        type: Boolean,
-        default: false,
-      },
-      size: {
-        type: String,
-        validator: function (value) {
-          return ['small', 'medium', 'large'].indexOf(value) !== -1;
-        },
-      },
-      backgroundColor: {
-        type: String,
+export default {
+  name: 'NInput',
+
+  props: {
+    label: {
+      type: String,
+    },
+    small: {
+      type: String,
+    },
+    basic: {
+      type: Boolean,
+    },
+    size: {
+      type: String,
+      validator: function (value) {
+        return ['small', 'medium', 'large'].includes(value);
       },
     },
-  
-    emits: ['click'],
-  
-    setup(props, { emit }) {
-      props = reactive(props);
+  },
+
+  emits: ['click'],
+
+  data() {
+    return {
+      // Reactive data can be defined here if needed
+    };
+  },
+
+  computed: {
+    inputClasses() {
       return {
-        classes: computed(() => ({
-          'n-input__input ': true,
-          'n-input__input--primary': props.primary,
-          'n-input__input--secondary': !props.primary,
-        //   [`storybook-button--${props.size || 'medium'}`]: true,
-        })),
-        onClick() {
-          emit('click');
-        },
+        'n-input__input': true,
+        'n-input__input--default': !this.basic,
+        'n-input__input--basic': this.basic,
       };
     },
-  };
-  </script>
+  },
+
+  methods: {
+    onClick() {
+      this.$emit('click');
+    },
+  },
+};
+</script>
   
